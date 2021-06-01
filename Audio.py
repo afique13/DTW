2dda5fd117b5eaabc90e6ed02558ad39143795cb
Afiq Azizul <afique13@gmail.com>
2021-06-02 03:28:43 +0800

    audio comparison test

--- Audio.py
+++ Audio.py
@@ -7,31 +7,24 @@ import librosa.display
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 
-y1, sr1 = librosa.load('Audio File\JnTx1.wav')
-y2, sr2 = librosa.load('Audio File\JnTx2.wav')
+y1, sr1 = librosa.load('Audio File\JnTExpressx1.wav')
+y2, sr2 = librosa.load('Audio File\JnTExpressx0.5.wav')
+y3, sr3 = librosa.load('Audio File\MemohonMaafx1.wav')
+
 y_harmonic1, y_percussive1 = librosa.effects.hpss(y1)
 y_harmonic2, y_percussive2 = librosa.effects.hpss(y2)
+y_harmonic3, y_percussive3 = librosa.effects.hpss(y3)
 
 mfcc1 = librosa.feature.mfcc(y=y_percussive1, sr=sr1, n_mfcc=13)
 mfcc2 = librosa.feature.mfcc(y=y_percussive2, sr=sr2, n_mfcc=13)
-
-#print(mfcc1)
-#print(mfcc2)
-
-# plt.figure(figsize=(14,5))
-
-# plt.plot(y_percussive1)
-# plt.xlabel('Time (samples)')
-# plt.ylabel('Amplitude')
-# plt.show()
-
-# plt.figure(figsize=(14,5))
-# plt.plot(y_percussive2)
-# plt.xlabel('Time (samples)')
-# plt.ylabel('Amplitude')
-# plt.show()
+mfcc3 = librosa.feature.mfcc(y=y_percussive3, sr=sr3, n_mfcc=13)
 
 x = np.amax(mfcc1, axis=1)
 y = np.amax(mfcc2, axis=1)
-distance, path = fastdtw(x, y, dist=euclidean)
-print(distance)
+z = np.amax(mfcc3, axis=1)
+
+distance1, path1 = fastdtw(x, y, dist=euclidean)
+distance2, path2 = fastdtw(x, z, dist=euclidean)
+
+print(distance1)
+print(distance2)
