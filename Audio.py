b603246c3093c2409c04048478241d7390a989fa
Afiq Azizul <afique13@gmail.com>
2021-06-18 12:50:46 +0800

    string split add

--- Audio.py
+++ Audio.py
@@ -10,9 +10,16 @@ from DTW import dtw
 
 x = ''
 
-y1, sr1 = librosa.load('Audio File\JnTExpressx1.wav')
-y2, sr2 = librosa.load('Audio File\JnTExpressx0.5.wav')
-y3, sr3 = librosa.load('Audio File\MemohonMaafx1.wav')
+start = 'Audio File"\"'
+end = '.wav'
+
+ref = 'Audio File\JnTExpressx1.wav'
+test1 = 'Audio File\JnTExpressx0.5.wav'
+test2 = 'Audio File\MemohonMaafx1.wav'
+
+y1, sr1 = librosa.load(ref)
+y2, sr2 = librosa.load(test1)
+y3, sr3 = librosa.load(test2)
 
 y_harmonic1, y_percussive1 = librosa.effects.hpss(y1)
 y_harmonic2, y_percussive2 = librosa.effects.hpss(y2)
@@ -34,5 +41,5 @@ print()
 print(y_matrix)
 print()
 
-print("The distance between JntExpressx1 and JntExpressx0.5 is equal to ", x_matrix[x_matrix.shape[0]-1,x_matrix.shape[1]-1])
-print("The distance between JntExpressx1 and MemohonMaafx1 is equal to ", y_matrix[y_matrix.shape[0]-1,y_matrix.shape[1]-1])
+print("The distance between", ref[ref.find(start)+len(start):ref.rfind(end)],"and", test1[test1.find(start)+len(start):test1.rfind(end)],"is equal to", x_matrix[x_matrix.shape[0]-1,x_matrix.shape[1]-1])
+print("The distance between", ref[ref.find(start)+len(start):ref.rfind(end)],"and", test2[ref.find(start)+len(start):test2.rfind(end)],"is equal to", y_matrix[y_matrix.shape[0]-1,y_matrix.shape[1]-1])
